90634dd81bfcd6996a912b0df6e19a1b172f01b6
violasptntels <violaseptiantiana23@gmail.com>
2024-10-15 16:12:10 +0700

    Pratikum ke 2

---
+++ Pertemuan02/Pratikum/P2_1_714230001/P2_1_714230001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Masukan angka1: ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukan angka2: ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(input1+" + "+input2+" = "+(input1 + input2));
+            Console.WriteLine(input1+" - "+input2+" = "+(input1 - input2));
+            Console.WriteLine(input1+" * "+input2+" = "+(input1 * input2));
+            Console.WriteLine(input1+" / "+input2+" = "+(input1  / input2));
+            Console.ReadKey();
+        }
+    }
+}
